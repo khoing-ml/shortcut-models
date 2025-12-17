a2c79e22f9a54dfd7b333b55a0d4ae425522e839
khoing-ml <khoingmh@gmail.com>
2025-12-17 20:55:36 +0700

    hehe

--- train_local_no_bootstrap_locality.py
+++ train_local_no_bootstrap_locality.py
@@ -220,7 +220,8 @@ def main(_):
         del replace_dict['opt_state'] # Debug
         train_state = train_state.replace(**replace_dict)
         start_step = int(train_state.step.item())
-        train_state = jax.jit(lambda x : x, out_shardings=train_state_sharding)(train_state)
+        # Skip resharding after loading to avoid shape mismatch errors
+        # train_state = jax.jit(lambda x : x, out_shardings=train_state_sharding)(train_state)
         print("Loaded model with step", train_state.step)
         jax.debug.visualize_array_sharding(train_state.params['FinalLayer_0']['Dense_0']['kernel'])
         del cp
